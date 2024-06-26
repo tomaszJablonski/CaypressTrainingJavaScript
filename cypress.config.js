20728e4d58bda260995d1834376c7d86bcfc2780
https://github.com/tomaszJablonski <tjablonski1990@gmail.com>
2024-06-26 19:43:18 +0200

    first lesson

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      // defaultCommandTimeout :3000
       // implement node event listeners here
     },
   },
--- cypress/e2e/myFirstTest.cy.js
+++ cypress/e2e/myFirstTest.cy.js
@@ -1,5 +1,16 @@
 /// <reference types="cypress" />
 
-it('Google Search', () =>{
+it('automationstepbystep site', () =>{
     cy.visit('https://www.google.pl/')
+    cy.get('#L2AGLb > .QS5gu').click();
+    // cy.get('#APjFqb').type("Automation Step by Step").type('{enter}')
+    //albo tak jak poniżej lekko srócony zapis
+    cy.get('#APjFqb').type("Automation Step by Step{enter}")
+    /* cy.get('.className, {timeout:5000}').type('123 {enter}') 
+    - możemy ustawić większe czekanie ręcznie lub w pliku cypress.config.js dla wszystkich zwiększyć.
+    To ustawienie tutaj ma większy priorytet niż w configu         
+    albo jeszcze tak można 
+    cy.wiat(2000)
+    */
+
 })
